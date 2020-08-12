931f9c8487a557b8b28de46d44180a5016720b50
Md. Faiyaj Zaman <faiyajz007@gmail.com>
2020-08-12 10:59:42 +0600

    sidebar options with icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
+
 
 function App() {
   return (
     <div className="app">
       <h1> Hey Faiyaj</h1>
+
+    <Sidebar/>
+
+
     </div>
   );
 }
